ee2fe08bf59e9d930bee60dd6c8b64aea1d98478
Thierno IB. BARRY <ibrahima.br@gmail.com>
2016-06-22 23:02:08 +0200

    Add parse_plugin_repo filter.
    This filter is taken from the elasticsearch_plugin module for ansible

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -19,8 +19,28 @@ def append_to_list(values=[], suffix=''):
 def array_to_str(values=[],separator=','):
     return separator.join(values)
 
+def parse_plugin_repo(string):
+    elements = string.split("/")
+    ''' We first consider the simplest form: pluginname'''
+    repo = elements[0]
+    
+    '''We consider the form: username/pluginname'''
+    if len(elements) > 1:
+        repo = elements[1]
+    '''
+    remove elasticsearch- prefix
+    remove es- prefix
+    '''
+    for string in ("elasticsearch-", "es-"):
+        if repo.startswith(string):
+            return repo[len(string):]
+    return repo
+
 class FilterModule(object):
     def filters(self):
-        return {'modify_list': modify_list,
-        'append_to_list':append_to_list,
-        'array_to_str':array_to_str}
+        return {
+            'modify_list': modify_list,
+            'append_to_list': append_to_list,
+            'array_to_str': array_to_str,
+            'parse_plugin_repo': parse_plugin_repo
+        }
