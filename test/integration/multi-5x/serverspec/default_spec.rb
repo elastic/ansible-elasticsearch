0e2a0a5e995ef3fec42c3b8e62c56a82f59755fe
Dale McDiarmid <dalem@elastic.co>
2017-08-16 11:05:40 +0100

    Updated init, systemd, jvm.options and tests

--- test/integration/multi-5x/serverspec/default_spec.rb
+++ test/integration/multi-5x/serverspec/default_spec.rb
@@ -2,7 +2,7 @@ require 'multi_spec'
 
 
 describe 'Multi Tests v 5.x' do
-  include_examples 'multi::init', "5.2.2", ["ingest-geoip"]
+  include_examples 'multi::init', "5.5.1", ["ingest-geoip"]
 end
 
 
--- test/integration/xpack-5x/serverspec/default_spec.rb
+++ test/integration/xpack-5x/serverspec/default_spec.rb
@@ -1,5 +1,5 @@
 require 'xpack_spec'
 
 describe 'Xpack Tests v 5.x' do
-  include_examples 'xpack::init', "5.2.2", ["ingest-attachment"]
+  include_examples 'xpack::init', "5.5.1", ["ingest-attachment"]
 end
