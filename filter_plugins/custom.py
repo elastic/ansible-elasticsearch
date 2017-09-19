e9a6f74d09302217bc61715fdc1c05897f91cf9f
Dale McDiarmid <dalem@elastic.co>
2017-09-19 20:11:09 +0100

    Fix for issue #369

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -21,10 +21,10 @@ def append_to_list(values=[], suffix=''):
 def array_to_str(values=[],separator=','):
     return separator.join(values)
 
-def extract_role_users(users={}):
+def extract_role_users(users={},exclude_users=[]):
     role_users=[]
     for user,details in users.iteritems():
-        if "roles" in details:
+        if user not in exclude_users and "roles" in details:
             for role in details["roles"]:
                 role_users.append(role+":"+user)
     return role_users
@@ -32,19 +32,26 @@ def extract_role_users(users={}):
 def filename(filename=''):
     return os.path.splitext(os.path.basename(filename))[0]
 
-def filter_reserved(user_roles={}):
+def remove_reserved(user_roles={}):
     not_reserved = []
     for user_role,details in user_roles.items():
         if not "metadata" in details or not "_reserved" in details["metadata"] or not details["metadata"]["_reserved"]:
             not_reserved.append(user_role)
     return not_reserved
 
+def filter_reserved(users_role={}):
+    reserved = []
+    for user_role,details in users_role.items():
+        if "metadata" in details and "_reserved" in details["metadata"] and details["metadata"]["_reserved"]:
+            reserved.append(user_role)
+    return reserved
 
 class FilterModule(object):
     def filters(self):
         return {'modify_list': modify_list,
         'append_to_list':append_to_list,
+        'filter_reserved':filter_reserved,
         'array_to_str':array_to_str,
         'extract_role_users':extract_role_users,
-        'filter_reserved':filter_reserved,
+        'remove_reserved':remove_reserved,
         'filename':filename}
