23e336833a762517fb2d4e1b4c89431e2f25889c
Michael Russell <michael.russell@elastic.co>
2018-06-15 17:37:53 +0200

    This file should now be a symbolic link, temporarily removing to let the other tests run

--- test/integration/helpers/serverspec/standard_spec.rb
+++ test/integration/helpers/serverspec/standard_spec.rb
@@ -72,10 +72,6 @@ shared_examples 'standard::init' do |vars|
     end
   end
 
-  describe file('/usr/lib/systemd/system/elasticsearch.service') do
-    it { should_not exist }
-  end
-
   describe file('/etc/elasticsearch/elasticsearch.yml') do
     it { should_not exist }
   end
